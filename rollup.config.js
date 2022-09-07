d1e9335dba8557b3f4b67d36c39e7ef9a1cc837e
Lob2018 <@>
2022-09-07 17:29:45 +0200

    Feat : Better bundling
    Minified with rollup plugin tercer
    Avoid duplicate declaration with tslib

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,35 @@
-import sass from 'rollup-plugin-sass'
-import typescript from 'rollup-plugin-typescript2'
-import pkg from './package.json'
+import sass from "rollup-plugin-sass";
+import typescript from "rollup-plugin-typescript2";
+import pkg from "./package.json";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { terser } from 'rollup-plugin-terser';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  input: 'src/lib/index.tsx',
+  input: "src/lib/index.tsx",
   output: [
     {
       file: pkg.main,
-      format: 'cjs',
-      exports: 'named',
+      format: "cjs",
+      exports: "named",
       sourcemap: true,
-      strict: false
-    }
+      strict: false,
+      compact: true,
+    },
   ],
   plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ useTsconfigDeclarationDir: true }),
     sass({
       output: "./dist/style.css",
       failOnError: true,
       runtime: require("sass"),
     }),
-    typescript({ objectHashIgnoreUnknownHack: true })
+    terser()
   ],
-  external: ['react', 'react-dom']
-}
+  external: ["react", "react-dom"],
+};
